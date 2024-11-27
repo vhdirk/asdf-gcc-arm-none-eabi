165027523d7bbcea81bd41040ce49968fe9818f5
Dirk Van Haerenborgh <vhdirk@gmail.com>
2024-11-27 13:31:46 +0100

    make semver compatible-ish

--- lib/utils.bash
+++ lib/utils.bash
@@ -34,48 +34,57 @@ get_version() {
 
 }
 
-get_links() {
-	local content
-	local arch
-	local links
-	content=$1
 
-	# Get the system architecture
-	arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+all_links() {
+  local arch
+  local content
+  local links
+  local version
+
+  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+	content="$(curl -s "$URL")$(curl -s "$LEGACY_URL")"
 
 	# Fetch the content of the URL and extract links
-	links=$(echo "$content" | awk -F 'href="' '/<a/{gsub(/".*/, "", $2); print $2}' | grep -v "srcrel" | awk '!/-src./' | grep -E 'arm-none-eabi.*\.(tar.xz|tar.bz2|zip)\?' | awk -F '?' '{print $1}')
+	links=$(echo "$content" | \
+    awk -F 'href="' '/<a/{gsub(/".*/, "", $2); print $2}' | \
+    grep -v "srcrel" | \
+    awk '!/-src./' | \
+    grep -E 'arm-none-eabi.*\.(tar.xz|tar.bz2|zip)\?' | \
+    awk -F '?' '{print $1}' | \
+    grep -v -i -E "mingw|darwin|win32|mac")
 
-	declare -A versions
+	declare -A versions=()
 
-	while IFS= read -r link; do
-
-		local version
-		version=$(get_version "$link")
+  while IFS= read -r link; do
+    version=$(get_version "$link")
 
 		if [[ "$OSTYPE" =~ darwin* && "$link" =~ (darwin|mac) ]]; then
 			# filter mac links on mac
 			if [[ $link =~ $arch ]]; then
-				versions["$version"]=$BASE_URL$link
+				versions["$version"]="$link"
 			elif [[ "$arch" == "arm64" && ! -v versions["$version"] ]]; then
 				# The archs do not match. If we're on arm64, x86_64 is also usable
 				# first test if there already is a matching link, if not, fill it in
-				versions["$version"]=$BASE_URL$link
+				versions["$version"]="$link"
 			fi
 		elif [[ "$OSTYPE" =~ (cygwin|msys) && "$link" =~ (mingw|win32) ]]; then
-			versions["$version"]=$BASE_URL$link
+			versions["$version"]="$link"
 		elif [[ "$OSTYPE" =~ linux-gnu && ! "$link" =~ (mingw|darwin|win32|mac) ]]; then
 			# if nothing matches, nothing matches :)
 			if [[ "$link" =~ $arch ]]; then
-				versions[$version]=$BASE_URL$link
+				versions["$version"]="$link"
 			elif [[ $arch == "x86_64" && ! "$link" =~ "aarch64" ]]; then
-				versions[$version]=$BASE_URL$link
+				versions["$version"]="$link"
 			fi
 		fi
 	done <<<"$links"
 
-	declare -p versions
+  for version in "${!versions[@]}"; do
+    echo "${BASE_URL}${versions[$version]}"
+  done
 }
+
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -86,26 +95,83 @@ sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' | LC_ALL=C sort -V | awk '{print $2}'
 }
 
-list_all_versions() {
-	local content
-	content="$(curl -s "$URL")$(curl -s "$LEGACY_URL")"
-	local result
-	result=$(get_links "$content")
-	eval "$result"
+# ARM compilers are not semver. So we're just going to invent something here
+arm_version_to_semver() {
+  local version="$1"
+  version=${version//[_-]/.}
+  # shellcheck disable=SC2001
+  version=$(echo "$version" | sed 's/\([0-9]\)\([a-zA-Z]\)/\1.\2/')
+  version=${version//q/}
+  version=${version//rel1/0}
+  version=${version//bet1/0-beta1}
 
-	printf "%s\n" "${!versions[@]}"
+  if [[ "$version" =~ "mpacbti" ]]; then
+    version="${version//mpacbti./}-mpacbti"
+  fi
+  cut -d'.' -f1-3 <<< "$version"
 }
 
-release_filename() {
-	local content
-	content="$(curl -s "$URL")$(curl -s "$LEGACY_URL")"
-	local result
-	result=$(get_links "$content")
-	eval "$result"
+semver_to_arm_version() {
+  local version="$1"
+  #replace all dots wit globs, remove mpacbti string
+  version="${version//./\*}"
+  version="${version//-mpacbti/}"
+  echo "$version"
+}
 
+
+# shellcheck disable=SC2120
+list_all_versions() {
+  local links
+  local arch
+  local version=""
+  local semver=""
+  local versions=()
+
+  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+  links=$(all_links "$arch")
+
+  while IFS= read -r link; do
+    version="$(get_version "$link")"
+    semver="$(arm_version_to_semver "$version")"
+
+    # shellcheck disable=SC2190
+    versions+=( "$semver" )
+  done <<<"$links"
+
+  echo "${versions[@]}" | tr ' ' "\n" #| sort --version-sort
+}
+
+get_link() {
+  local version="$1"
+	local content
+	local arch
+	local links
+  local link_version
+
+  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+  while IFS= read -r link; do
+    link_version="$(get_version "$link")"
+    semver="$(arm_version_to_semver "$link_version")"
+
+    if [ "$semver" = "$version" ]; then
+      echo "$link"
+      return 0
+    fi
+  done <<<"$(all_links "$arch" )"
+}
+
+
+release_filename() {
 	local version
 	version="$1"
-	basename "${versions["$version"]}"
+
+  local url
+  url=$(get_link "${version}")
+
+	basename "${url}"
 }
 
 download_release() {
@@ -113,21 +179,16 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	local content
-	content="$(curl -s "$URL")$(curl -s "$LEGACY_URL")"
-	local result
-	result=$(get_links "$content")
-	eval "$result"
-
-	url=${versions["$version"]}
+  url=$(get_link "${version}")
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 latest_release() {
-	list_all_versions | sort_versions | tail -n1 | xargs echo
+	list_all_versions | sort --version-sort
 }
+
 
 install_version() {
 	local install_type="$1"
